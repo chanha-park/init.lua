ce87f6068f0eef113d8cb3186df38d6a194941c1
chanhpar <chanhpar@student.42seoul.kr>
2023-10-30 02:36:19 +0900

    disable lazy.nvim's plugin update checker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -46,7 +46,7 @@ require('lazy').setup({
             lazy = '💤 ',
         },
     },
-    checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+    checker = { enabled = false, notify = false }, -- automatically check for plugin updates
     change_detection = { enabled = false },
     performance = {
         rtp = {
