5e272101d187d0259ca553cff6db3e2a3d21fb8a
chanha-park <chanha.dev@gmail.com>
2023-11-28 20:48:06 +0900

    Add autocmd to prevent foldexpr issue

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,3 +17,12 @@ vim.api.nvim_create_autocmd('CompleteDone', {
     group = vim.api.nvim_create_augroup('ClosePopUpMenu', { clear = true }),
     pattern = '*',
 })
+
+-- XXX: refer https://github.com/neovim/neovim/issues/20726
+vim.api.nvim_create_autocmd('TermOpen', {
+    callback = function()
+        vim.opt.foldmethod = 'manual'
+    end,
+    group = vim.api.nvim_create_augroup('PreventHang', { clear = true }),
+    pattern = '*',
+})
