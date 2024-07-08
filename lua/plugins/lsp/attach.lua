f5c5fd70035427b3a116af225fbd2621964cc3a9
chanha-park <chanha.dev@gmail.com>
2024-07-08 10:13:11 +0900

    add lsp keymaps

--- lua/plugins/lsp/attach.lua
+++ lua/plugins/lsp/attach.lua
@@ -9,26 +9,38 @@ return function(_, bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
+    local imap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('i', keys, func, { buffer = bufnr, desc = desc })
+    end
+
     nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
     nmap('<leader>I', function()
         vim.lsp.buf.code_action({ context = { only = quickfix } })
     end, '[C]ode [A]ction')
 
     nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('gD', vim.lsp.buf.type_definition, '[G]oto Type [D]efinition')
     nmap(
         'gr',
         require('telescope.builtin').lsp_references,
         '[G]oto [R]eferences'
     )
     nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
     -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
     -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    -- nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    imap('<A-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    nmap('<A-k>', function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+    end, 'Toggle inlay hint')
 
     -- Lesser used LSP functionality
     -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
