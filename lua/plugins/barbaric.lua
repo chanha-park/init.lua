a916de7219234fd929b2c6fad972f0fcb5248752
chanha-park <chanha.dev@gmail.com>
2024-07-10 18:50:43 +0900

    update vim-barbaric config

--- lua/plugins/barbaric.lua
+++ lua/plugins/barbaric.lua
@@ -1,8 +1,22 @@
+local input_methods = { 'ibus', 'fcitx', 'kime' }
+local input_method = nil
+
+for _, im in ipairs(input_methods) do
+    if vim.fn.executable(im) then
+        input_method = im
+        break
+    end
+end
+
+if not input_method then
+    return {}
+end
+
 if vim.uv.os_uname().sysname == 'Linux' then
     return {
         'rlue/vim-barbaric',
         config = function()
-            vim.g.barbaric_ime = 'fcitx'
+            vim.g.barbaric_ime = input_method
         end,
     }
 else
