2fdb300451f2cfcb17e0a3f187873186fc2d6fee
chanha-park <chanha.dev@gmail.com>
2024-08-05 18:45:27 +0900

    refactor lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,22 +9,22 @@ return {
     config = function()
         -- [[ Configure LSP ]]
         --  This func gets run when an LSP connects to a particular buffer.
-        local on_attach = require('plugins.lsp.attach')
+        local default_on_attach = require('plugins.lsp.attach')
         local servers = require('plugins.lsp.servers')
+        local lspconfig = require('lspconfig')
 
         -- ref: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#cssls
         --Enable snippet capability for completion
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.completion.completionItem.snippetSupport =
+        local default_client_cap = vim.lsp.protocol.make_client_capabilities()
+
+        default_client_cap.textDocument.completion.completionItem.snippetSupport =
             true
 
         for server, conf in pairs(servers) do
-            conf['on_attach'] = conf['on_attach'] or on_attach
+            conf['on_attach'] = conf['on_attach'] or default_on_attach
+            conf['capabilities'] = conf['capabilities'] or default_client_cap
 
-            -- if conf['capabilities'] == nil then
-            --     conf['capabilities'] = capabilities
-            -- end
-            require('lspconfig')[server].setup(conf)
+            lspconfig[server].setup(conf)
         end
 
         -- Ensure the servers above are installed
