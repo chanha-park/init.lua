2fc21395b7e12e6feb1f40e1a123fd10601509b3
chanha-park <chanha.dev@gmail.com>
2026-04-13 20:07:17 +0900

    update deprecated api in lspconfig.lua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -11,27 +11,25 @@ return {
         --  This func gets run when an LSP connects to a particular buffer.
         local default_on_attach = require('plugins.lsp.attach')
         local servers = require('plugins.lsp.servers')
-        local lspconfig = require('lspconfig')
 
-        -- ref: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#cssls
-        --Enable snippet capability for completion
-        local default_client_cap = vim.lsp.protocol.make_client_capabilities()
-
-        default_client_cap.textDocument.completion.completionItem.snippetSupport =
-            true
+        vim.lsp.config('*', { on_attach = default_on_attach })
 
         for server, conf in pairs(servers) do
-            conf['on_attach'] = conf['on_attach'] or default_on_attach
-            conf['capabilities'] = conf['capabilities'] or default_client_cap
+            local preset_on_attach = vim.lsp.config[server].on_attach
+            local own_on_attach = conf['on_attach'] or default_on_attach
 
-            lspconfig[server].setup(conf)
+            conf['on_attach'] = function(client, buffer)
+                preset_on_attach(client, buffer)
+                own_on_attach(client, buffer)
+            end
+
+            vim.lsp.config(server, conf)
+            vim.lsp.enable(server)
         end
 
         -- Ensure the servers above are installed
         require('mason-lspconfig').setup({
             -- ensure_installed = vim.tbl_keys(servers),
         })
-
-        -- require('plugins.lsp.autoformat')
     end,
 }
